889bb5fafc03f1cd5756aec66577937bfe1175e1
Sen Chen <senchen@CNsenchen.local>
2018-07-18 22:12:43 +0800

    解决一个数同时是几个数的倍数的情况

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,19 @@
 function fizzbuzz() {
     for(let i=1; i<120; i++) {
+        let added = "";
         if(i % 3 == 0) {
-            console.log('Fizz');
-        } else if(i % 5 == 0) {
-            console.log('Buzz');
-        } else if(i % 7 == 0) {
-            console.log('Whizz');
-        } else {
+            added += 'Fizz';
+        }
+        if(i % 5 == 0) {
+            added += 'Buzz';
+        }
+        if(i % 7 == 0) {
+            added += 'Whizz';
+        } 
+        if(added == "") {
             console.log(i);
+        } else {
+            console.log(added);
         }
     }
 }
